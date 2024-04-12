6c65d6778f263b3927fdbcac2aa378732c9145b9
Carl Capodice <awsomesawce@outlook.com>
2024-04-12 13:07:45 -0400

    feat: :sparkles: Added more aliases to aliases.fish
    Will be getting rid of the files in to_merge/ after they are fully merged.

--- aliases.fish
+++ aliases.fish
@@ -1,9 +1,10 @@
 #!/usr/bin/env fish
 # General fish aliases and functions
 
+# chtsh for fish that accepts multiple args.
 function cht -d "chtsh for fish"
-    set -f chturl https://cht.sh
-    set -f cnt (count $argv)
+    set -f chturl https://cht.sh # chtsh url
+    set -f cnt (count $argv) # Number of args
 
     # Test whether curl is installed
     if not command -q curl
@@ -20,7 +21,7 @@ function cht -d "chtsh for fish"
         command -q curl
         and command curl -sS "$chturl/$argv"
     else
-        echo "No args specified"
+        echo "No args specified" >&2
         command curl -sS "$chturl"
     end
 
@@ -46,10 +47,43 @@ function e -d "eza shortcut" -a
 end
 
 alias g=git
+alias gitst="git status"
 alias pm=pacman
 alias adoc=asciidoctor
-alias dr="deno run"
-alias dra="deno run --allow-all"
-alias ddoc="deno doc"
-alias dlint="deno lint"
-alias dfmt="deno fmt"
+
+# deno specific aliases are only set if deno is installed.
+if command -q deno
+    alias dr="deno run"
+    alias dra="deno run --allow-all"
+    alias ddoc="deno doc"
+    alias dlint="deno lint"
+    alias dfmt="deno fmt"
+end
+
+# Added command checks 04/12/2024
+if command -q poetry
+    alias poe="poetry"
+end
+
+if command -q invoke
+    alias inv=invoke
+else
+    alias inv="python -m invoke"
+end
+
+alias pydoc="python -m pydoc"
+alias fire="python -m fire"
+
+function pysysconfig -d "Output of sysconfig module"
+    command -q python; and command python -m sysconfig
+end
+
+function rich_md -d "Using rich.markdown from the commandline"
+    if not command -q python
+        echo "python not installed dummy!" >&2
+        return 2
+    end
+
+    echo "You have $(count $argv) args on this function!"
+    python -m rich.markdown $argv
+end
